7216d3172983adc33ec3b77d049d69073bd7a976
uriah rokach <uriahrokach>
2021-12-13 02:02:42 +0200

    Fixed warnings

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { getFirestore, collection } from 'firebase/firestore';
 
 import React from 'react';
 import Home from './pages/home';
-import logo from './logo.svg';
 // import './App.css';
 
 const firebaseConfig = {
@@ -13,10 +12,10 @@ const firebaseConfig = {
   storageBucket: "giftshop-2243d.appspot.com",
   messagingSenderId: "27326949595",
   appId: "1:27326949595:web:45da994bcd2c27e37284fe",
-  measurementId: "${config.measurementId}"
+  measurementId: "\${config.measurementId}"
 };
 
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 const firestore = getFirestore() 
 
 function App() {
