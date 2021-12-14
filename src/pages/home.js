aa44bfff98176ec74770eb1c0830b5e4ec886c14
uriah rokach <uriahrokach>
2021-12-14 15:54:39 +0200

    Added styling for web

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,12 @@
-import React from "react"
-import { useState } from "react";
+import React, { useState } from "react";
 import { onSnapshot, collection, query, where, orderBy } from "firebase/firestore";
+import logo from '../images/logo.png';
 import FilterBar from "../components/filterbar";
 
+import '../styles/home.css'
+
 const queryGifts = (value, tags, db, setGifts) => {
-    let gifts = null
+    let gifts = null;
     if (tags.length != 0) {
         gifts = query(
             collection(db, "gifts"),
@@ -30,12 +32,13 @@ const queryGifts = (value, tags, db, setGifts) => {
 
 const Gift = (props) => {
     return (
-        <div>
-            <h3>{props.gift.name} <small>(Rank {props.gift.rank})</small></h3>
+        <div className="gift-card">
+            <div className="gift-title">{props.gift.name} <small>(rank {props.gift.rank})</small></div>
             <div>{props.gift.description}</div>
-            <div><b>{props.gift.mechanics}</b></div>
             <br />
-            {props.gift.tags.map(tag => <div>{tag}</div>)}
+            <div><b>System:</b> {props.gift.mechanics}</div>
+            <br />
+            {props.gift.tags.map(tag => <div className="tag">{tag}</div>)}
         </div>
     )
 }
@@ -46,16 +49,15 @@ const Home = (props) => {
     const [tags, setTags] = useState([]);
     queryGifts(value, tags, props.db, setGifts)
     return (
-        <div>
-            <div>
-                 <FilterBar tags={tags} setTags={setTags}/>
-            </div>
-            <p>{tags.join(', ')}</p>
-            <div>
-                <input type="text" onChange={(e) => setValue(e.target.value)}/>
+        <div className="home">
+            <FilterBar tags={tags} setTags={setTags}/>
+            <div className="main-body">
+                <div className="main-header">
+                    <img src={logo} alt="logo" />
+                    <input type="text" className="gift-search" placeholder="search for gifts, i.e. resist pain, razor claws... " onChange={(e) => setValue(e.target.value)}/>
+                </div>
                 {gifts.map(gift => <Gift gift={gift} />)}
             </div>
-            
         </div>
     )
 }
